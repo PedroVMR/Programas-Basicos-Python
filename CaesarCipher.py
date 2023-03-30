8e7cb1c9a1ab9f79aa342ab4bdbf1ba896cd1861
Pedro Reis <125422568+PedroVMR@users.noreply.github.com>
2023-03-30 17:15:33 -0300

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,38 +1,77 @@
+from replit import clear
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Digite 'encode' para Criptografar, digite 'decode' para Descriptografar:\n")
-text = input("Digite a mensagem:\n").lower()
-shift = int(input("Digite o número de substituição:\n"))
+code_on = True
 
-def encrypt(plain_text, shift_amount):
-    encrypt_text = ""
-    for char in plain_text:
-        if char in alphabet:
-            char_index = alphabet.index(char)
-            encrypt_text += alphabet[(char_index + shift_amount) % len(alphabet)]
-        else:
-            encrypt_text += char
+while code_on:
+    code_on = True
 
-    print(f"A mensagem é: {encrypt_text}")
-    return encrypt_text
-       
-if direction == "encode":
-    encrypted_text = encrypt(plain_text=text, shift_amount=shift)
-    user_choice = input("Você deseja descriptografar a mensagem [S/N]: ").lower()
-    if user_choice == "s":
-        cipher_password = int(input("Digite o número correto da cifra: "))
-        if cipher_password == shift:
-            encrypt(plain_text = encrypted_text, shift_amount = - shift)
+    clear()
+    print(logo)
+    direction = input("Digite 'encode' para Criptografar, digite 'decode' para Descriptografar:\n")
+    text = input("Digite a mensagem:\n").lower()
+    shift = int(input("Digite o número de substituição:\n"))
 
-        else:
-            print("Senha Inválida")
+    def user_choiceEnd():
+        global code_on
+        user_choice_end = input("Você deseja rodar o código de novo [S/N]: ").lower()
+        if user_choice_end == "n":
+            code_on = False
+        elif user_choice_end == "s":
+            code_on = True
 
-    elif user_choice == "n":
-        print("Obrigado por usar o código ;D")
+    def encrypt(plain_text, shift_amount):
+        encrypt_text = ""
+        for char in plain_text:
+            if char in alphabet:
+                char_index = alphabet.index(char)
+                encrypt_text += alphabet[(char_index + shift_amount) % len(alphabet)]
+            else:
+                encrypt_text += char
 
-elif direction == "decode":
-    encrypt(plain_text = text, shift_amount = - shift)
+        print(f"A mensagem é: {encrypt_text}")
+        return encrypt_text
+        
+    if direction == "encode":
+        encrypted_text = encrypt(plain_text=text, shift_amount=shift)
+        user_choice = input("Você deseja descriptografar a mensagem [S/N]: ").lower()
+        if user_choice == "s":
+            cipher_password = int(input("Digite o número correto da cifra: "))
+            if cipher_password == shift:
+                encrypt(plain_text = encrypted_text, shift_amount = - shift)
+                user_choiceEnd()
 
-else:
-    print("Opção Inválida")
+            else:
+                print("Senha Inválida")
+                user_choiceEnd()
+
+        elif user_choice == "n":
+            print("Obrigado por usar o código ;D")
+            user_choiceEnd()
+
+    elif direction == "decode":
+        encrypt(plain_text = text, shift_amount = - shift)
+        user_choiceEnd()
+
+    else:
+        print("Opção Inválida")
+        user_choiceEnd()
